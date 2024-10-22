4e2c970be81649de0659745cd5784c833c1da977
Little Fish <63674966+Little-Fish-hub@users.noreply.github.com>
2024-10-22 16:09:56 -0600

    add examples

--- examples/scripts/appear_objects.gd
+++ examples/scripts/appear_objects.gd
@@ -1,52 +1,37 @@
 extends Node2D
 
-const box = preload("res://objects/box.tscn")
-const circle = preload("res://objects/circle.tscn")
-const heart = preload("res://objects/heart.tscn")
+const car = preload("res://objects/car.tscn")
 
 var ubi : Vector2
+var tt: bool = false
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	ubi = $ubiAppear.global_position
-	
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
 
+func _process(delta: float) -> void:
+	if tt:
+		$Node2D.scale = $Node2D.scale.lerp(Vector2(1, 1), 0.1)
+	pass
 
-func _on_gesture_gesture_name(gestureName: StringName) -> void:
+func _on_gesture_gesture_name(gestureName: StringName, distCloudPoint : float) -> void:
 	match gestureName:
-		"box" : create_box()
-		"circle" : create_circle()
-		"heart" : create_heart()
-	pass # Replace with function body.
+		"car" : create_car()
+		"sun" : create_sun()
+		"tree" : create_tree()
+	pass
 
-func create_box():
-	var instBox = box.instantiate()
-	instBox.position = ubi
-	add_child(instBox)
-	if !$CanvasLayer/Panel/VBoxContainer/Label4/MeshInstance2D.visible:
-		$CanvasLayer/Panel/VBoxContainer/Label4/MeshInstance2D.visible = true
+func create_car():
+	var instCar = car.instantiate()
+	instCar.position = ubi
+	add_child(instCar)
+	pass
 
-func create_circle():
-	var instCircle = circle.instantiate()
-	instCircle.position = ubi
-	add_child(instCircle)
-	if !$CanvasLayer/Panel/VBoxContainer/Label3/MeshInstance2D.visible:
-		$CanvasLayer/Panel/VBoxContainer/Label3/MeshInstance2D.visible = true
+func create_sun():
+	$Sun.visible = true
+	pass
 
-func create_heart():
-	var instHeart = heart.instantiate()
-	instHeart.position = ubi
-	add_child(instHeart)
-	if !$CanvasLayer/Panel/VBoxContainer/Label2/MeshInstance2D.visible:
-		$CanvasLayer/Panel/VBoxContainer/Label2/MeshInstance2D.visible = true
-
-
-func _on_gesture_on_draw_exit() -> void:
-	$Gesture.classify()
-	pass # Replace with function body.
+func create_tree():
+	tt = true
+	pass
